fb82bdeba8b81abb768bc6804f4673f5df7ed1aa
Shintaro2 <bbbtoa19930822@gmail.com>
2022-06-19 01:06:57 +0900

    Modelフォルダへの接続エラーの修正

--- app/Http/Controllers/SakenaviController.php
+++ app/Http/Controllers/SakenaviController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\sake;
+use App\Models\Sake;
 use App\Http\Requests\SakeRequest;
 
 class SakenaviController extends Controller
